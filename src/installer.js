cfa48eacc0337a362292bf2788f06241600875c4
Erik Golinelli <erik@codekraft.it>
2023-12-11 13:27:53 +0100

    Refactor codebase and bump version to 1.0.0
    The commits refactored the codebase by removing unnecessary semicolons and reformatted the code to match linting rules. The version of the TSDoc action has been updated from 0.0.1 to 1.0.0 in both `package.json` and `package-lock.json` files. The label on the workflow was also updated from "Build Docs" to "TSDoc Action" for better clarity.

--- src/installer.js
+++ src/installer.js
@@ -1,7 +1,7 @@
-const core = require('@actions/core');
-const exec = require('@actions/exec');
-const path = require('path');
-const utils = require('./utils');
+const core = require('@actions/core')
+const exec = require('@actions/exec')
+const path = require('path')
+const utils = require('./utils')
 
 /**
  * Installs a TSDoc template or theme by executing `npm install template/theme --production`.
@@ -10,42 +10,42 @@ const utils = require('./utils');
  * @async
  * @returns {Promise<string>} The name of the installed template or theme.
  */
-async function installTemplate(template) {
-  let templateName = '';
-  const actionDir = path.join(__dirname, '../');
-  core.debug(`actionDir: ${actionDir}`);
+async function installTemplate (template) {
+  let templateName = ''
+  const actionDir = path.join(__dirname, '../')
+  core.debug(`actionDir: ${actionDir}`)
 
-  let cmd = 'npm';
-  let args = ['install', template, '--production'];
-  core.info(`Installing TSDoc template/theme: ${template}`);
-  core.debug(`Command: ${cmd} ${args}`);
+  const cmd = 'npm'
+  const args = ['install', template, '--production']
+  core.info(`Installing TSDoc template/theme: ${template}`)
+  core.debug(`Command: ${cmd} ${args}`)
 
-  const options = {};
-  options.cwd = actionDir;
-  await exec.exec(cmd, args, options);
+  const options = {}
+  options.cwd = actionDir
+  await exec.exec(cmd, args, options)
 
-  let lsOutput = '';
-  let lsError = '';
+  let lsOutput = ''
+  let lsError = ''
   options.listeners = {
     stdout: (data) => {
-      lsOutput += data.toString();
+      lsOutput += data.toString()
     },
     stderr: (data) => {
-      lsError += data.toString();
-    },
-  };
+      lsError += data.toString()
+    }
+  }
 
-  await exec.exec('npm', ['ls', template, '-p'], options);
-  core.debug(`Template/theme location: ${lsOutput}`);
-  if (lsError) core.info(`Error: ${lsError}`);
-  let packageLocation = lsOutput.trim(); // path/to/template
+  await exec.exec('npm', ['ls', template, '-p'], options)
+  core.debug(`Template/theme location: ${lsOutput}`)
+  if (lsError) core.info(`Error: ${lsError}`)
+  const packageLocation = lsOutput.trim() // path/to/template
 
-  const filePath = path.join(packageLocation + '/package.json');
-  templateName = await utils.getPackageName(filePath);
+  const filePath = path.join(packageLocation + '/package.json')
+  templateName = await utils.getPackageName(filePath)
 
-  return templateName;
+  return templateName
 }
 
 module.exports = {
-  installTemplate,
-};
+  installTemplate
+}
